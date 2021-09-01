cc728b863b35a5f950ef7907f747a178a14881f5
Douglas Augusto <douglassan1@hotmail.com>
2021-09-01 16:57:53 -0300

    Commit: classes, objetos e definições feitas

--- src/conta.php
+++ src/conta.php
@@ -2,5 +2,18 @@
 
 class Conta
 {
-    
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo;
 }
+
+$primeiraConta->cpfTitular = "123.456.789-10";
+$primeiraConta->nomeTitular = "João Silva";
+$primeiraConta->saldo = 1500;
+$segundaConta->cpfTitular = "109.876.543-21";
+$segundaConta->nomeTitular = "Maria Lennon";
+$segundaConta->saldo = 5000;
+
+[$saldoJohn => $primeiraConta->saldo, $saldoMaria = $segundaConta->saldo];
+echo "Valor de Maria em conta = $saldoMaria" . PHP_EOL;
+echo "Valor de John em conta = $saldoJohn";
