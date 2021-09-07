49ecd9d9b45d53f28c6b8753b750b1a5528ace53
Douglas Augusto <douglassan1@hotmail.com>
2021-09-07 19:19:22 -0300

    Commit: comando this visto em aula alura, serve para pegar o endereço do objeto dentro da classe

--- src/conta.php
+++ src/conta.php
@@ -5,7 +5,22 @@ class Conta
     public $cpfTitular;
     public $nomeTitular;
     public $saldo;
+
+    public function sacar(float $valorASacar)
+    {
+        if ($this->saldo < $valorASacar)
+        {
+            echo "Você não pode sacar";
+        }
+        else
+        {
+            $this->saldo -= $valorASacar;
+        }
+    }
 }
+
+$primeiraConta = new Conta();
+$segundaConta = new Conta();
 
 $primeiraConta->cpfTitular = "123.456.789-10";
 $primeiraConta->nomeTitular = "João Silva";
@@ -14,6 +29,8 @@ $segundaConta->cpfTitular = "109.876.543-21";
 $segundaConta->nomeTitular = "Maria Lennon";
 $segundaConta->saldo = 5000;
 
-[$saldoJohn => $primeiraConta->saldo, $saldoMaria = $segundaConta->saldo];
+$saldoJohn = $primeiraConta->saldo;
+$saldoMaria = $segundaConta->saldo;
+
 echo "Valor de Maria em conta = $saldoMaria" . PHP_EOL;
 echo "Valor de John em conta = $saldoJohn";
