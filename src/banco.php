75864534c35a9bc2f107b64474956af3e4a33aa9
Douglas Augusto <douglassan1@hotmail.com>
2021-09-19 23:26:41 -0300

    Commit: finalizado sobre construtor e mencionado sobre o retirar dos setters, para impedir que os dados violem a lei do encapsulamento

--- src/banco.php
+++ src/banco.php
@@ -3,10 +3,14 @@
 require_once "src/conta.php";
 
 $primeiraConta = new Conta("123.456.789-10", "Douglas Augusto");
-var_dump($primeiraConta);
 $segundaConta = new Conta("123.456.789-09", "Stella Britto");
+
 $primeiraConta->deposita(1500);
 $primeiraConta->saca(300); 
-$primeiraConta->saca(400);
-echo $primeiraConta->mostraSaldo();
 
+echo $primeiraConta->mostraTitular() . PHP_EOL;
+echo $primeiraConta->mostraCpfDoTitular() . PHP_EOL;
+echo $primeiraConta->mostraSaldo() . PHP_EOL;
+
+$terceiraConta = new Conta("321.654.987.01", "Barney Cachorro");
+//var_dump($terceiraConta);
--- src/conta.php
+++ src/conta.php
@@ -68,18 +68,3 @@ class Conta
     }
 
 }
-
-
-
-// $primeiraConta->cpfTitular = "123.456.789-10";
-// $primeiraConta->nomeTitular = "João Silva";
-// $primeiraConta->saldo = 1500;
-// $segundaConta->cpfTitular = "109.876.543-21";
-// $segundaConta->nomeTitular = "Maria Lennon";
-// $segundaConta->saldo = 5000;
-
-// $saldoJohn = $primeiraConta->saldo;
-// $saldoMaria = $segundaConta->saldo;
-
-// echo "Valor de Maria em conta = $saldoMaria" . PHP_EOL;
-// echo "Valor de John em conta = $saldoJohn";
